a148bbe659ad60ff4d5b798849e92598c4c8feaf
Kat0x0Ace <240183070+Kat0x0Ace@users.noreply.github.com>
2026-04-16 16:31:26 -0400

    Setup a test agent with google gemini
    Added descriptive comments for each section, created a get_date tool to retrieve the current date, imported gemini-3-flash model using langchain, Tested the output response of the model and the tool calling functionality.

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from dotenv import load_dotenv
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import create_agent
+from datetime import datetime
 
+# Environment variables
 load_dotenv()
-
 google_api_key = os.getenv("GOOGLE_API_KEY")
 
+# Tools
+def get_date():
+    """Get the current date"""
+    return datetime.now().strftime("%m-%d-%Y")
+
+# System prompt for the model
+system_prompt = """
+You are a helpful assistant.
+Use the get_date tool if the user is asking about today's date.
+"""
+# User query
+user_query = input("Enter a query: ")
+
+# Create and configure the agent
+llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview")
+agent = create_agent(model=llm, tools=[get_date], system_prompt=system_prompt)
+response = agent.invoke({"messages":[{"role": "user", "content": user_query}]})
+
+# Print the models response
+print(response['messages'][-1].content[0]['text'])
