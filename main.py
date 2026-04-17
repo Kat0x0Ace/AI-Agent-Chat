97c7fa8d7ed9c4487dfdf8a4b75f9887934f2eec
Kat0x0Ace <240183070+Kat0x0Ace@users.noreply.github.com>
2026-04-17 15:23:38 -0400

    Added Gradio chat UI and refined agent
    Integrated a Gradio Blocks chat interface and replaced the CLI input/print flow with a cha t(message, history) handler. Refactored the agent creation formatting, updated the system prompt to be friendlier and to use the get_date tool only when explicitly asked, and keep the get_date utility for agent tools. Launched the Gradio demo on run to test.

--- main.py
+++ main.py
@@ -3,27 +3,41 @@ import os
 from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
 from datetime import datetime
+import gradio as gr
 
 # Environment variables
 load_dotenv()
 
-# Tools
+# Agent Tools
 def get_date():
     """Get the current date"""
     return datetime.now().strftime("%m-%d-%Y")
 
 # System prompt for the model
 system_prompt = """
-You are a helpful assistant.
-Use the get_date tool if the user is asking about today's date.
+You are a helpful assistant. 
+Answer all user queries in a kind manner.
+Use the get_date tool ONLY when the user is explicitly asking about today's date.
 """
-# User query
-user_query = input("Enter a query: ")
 
 # Create and configure the agent
 llm = ChatOllama(model="qwen3:8b")
-agent = create_agent(model=llm, tools=[get_date], system_prompt=system_prompt)
-response = agent.invoke({"messages":[{"role": "user", "content": user_query}]})
+agent = create_agent(
+    model=llm,
+    tools=[get_date],
+    system_prompt=system_prompt
+)
 
-# Print the models response
-print(response['messages'][-1].content)
+# Chat content
+def chat(message, history):
+    response = agent.invoke({"messages":[{"role": "user", "content": message}]})
+    last_response = response['messages'][-1].content
+    return last_response
+
+# Set up Gradio with a title and chat interface
+with gr.Blocks() as demo:
+    gr.Markdown('# AI Chatbot')
+    gr.ChatInterface(fn=chat)
+
+
+demo.launch()
